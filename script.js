2cad7823253479f67df83c130a8529823631b4a8
Ricardo Santos <112733103+Faltzin@users.noreply.github.com>
2025-08-09 13:13:11 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,18 +1,3 @@
-script.js
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>document.getElementById('meuBotao').addEventListener('click', function() {
+document.getElementById('meuBotao').addEventListener('click', function() {
     alert('Olá! Você clicou no botão!');
 });
-
-
-Adicionando meu primeiro projeto
